04210859ce5f38674cf7cd43844ec4c44dc850ec
Андрей Евстюхин <andrewe@playrix.com>
2016-12-08 12:04:56 +0300

    Подключил PVS-Studio Free

--- EtcCompress/EtcCompress.cpp
+++ EtcCompress/EtcCompress.cpp
@@ -1,3 +1,7 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+// --------------------------------------------------------------------------------
+//
 // Copyright(c) 2016 Playrix LLC
 //
 // LICENSE: https://mit-license.org
